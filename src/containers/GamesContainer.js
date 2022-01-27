6617dc86cb118431690287391896979454f2d393
kendra Hood <my.github.email@example.com>
2022-01-27 15:19:27 -0600

    clean up code

--- src/containers/GamesContainer.js
+++ src/containers/GamesContainer.js
@@ -1,31 +1,23 @@
 import { useState, useEffect } from "react"
-import  GameFilter  from "../components/games/GameFilter"
 import GamesList from "../components/games/GamesList"
 import React from 'react'
 
 const GamesContainer = () => {
     const [games, setGames] = useState([])
-    const [filteredGames, setFilteredGames] = useState(games)
+
 
     useEffect(() => {
         fetch("http://localhost:3000/games")
         .then(res => res.json())
         .then(data => setGames(data))
-        // .then(data => setFilteredGames(data))
         .catch(error => alert(error()))
         
     }, [])
 
 
-    const handleSearch = (searchValue) => {
-        const filteredGames = games.filter(game => games.name.toLowerCase()
-        .startsWith(searchValue.toLowerCase()))
-
-        setFilteredGames(filteredGames)
-    }
     return (
         <div>
-            {/* <GameFilter handleSearch={handleSearch} /> */}
+           
             <GamesList games={games}/>
         </div>
     )
